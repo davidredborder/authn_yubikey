6efd23b51831bf4636725e56a8d0832f99f286e7
Stefan Horst <stefan@ultrachaos.de>
2014-11-08 12:29:33 +0100

    init commit

---
+++ modules.mk
@@ -0,0 +1,6 @@
+SRC = mod_authn_yubikey.slo
+SRC_LO = mod_authn_yubikey.lo
+mod_authn_yubikey.la: $(SRC)
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version $(SRC_LO) 
+DISTCLEAN_TARGETS = modules.mk
+shared =  mod_authn_yubikey.la
